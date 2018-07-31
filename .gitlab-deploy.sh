e12e29bdd6e823dcea7965779722b18d81941f6a
Jenkinsadmin <s.hariharan@mcwaneservices.com>
2018-07-31 13:03:31 +0000

    Update .gitlab-deploy.sh kl

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -5,4 +5,5 @@ string=$DEPLOY_SERVER
 #Iterate servers for deploy and pull last commit
       echo "Deploy project on server ${array[i]}"    
       #ssh ubuntu@ec2-13-232-57-200.ap-south-1.compute.amazonaws.com "cd /opt && git clone http://Hariharan:SVR53S6usDk9ysxFDPB-@gitlab.com/mws-technology/MIGDataModel.git"
-      sshpass -f $SSH_PRIVATE_KEY  rsync -avz -e ssh charlie@13.232.57.200:"cd /opt && git clone http://Hariharan:SVR53S6usDk9ysxFDPB-@gitlab.com/mws-technology/MIGDataModel.git"
+      apt-get install -y sshpass
+      #sshpass -f $SSH_PRIVATE_KEY  rsync -avz -e ssh charlie@13.232.57.200:"cd /opt && git clone http://Hariharan:SVR53S6usDk9ysxFDPB-@gitlab.com/mws-technology/MIGDataModel.git"
