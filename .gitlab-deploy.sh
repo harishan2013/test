91b2d2996eee175ffd19493bb96b3eb77bc0e6a8
Jenkinsadmin <s.hariharan@mcwaneservices.com>
2018-07-31 12:54:30 +0000

    Update .gitlab-deploy.shv

--- .gitlab-deploy.sh
+++ .gitlab-deploy.sh
@@ -4,4 +4,4 @@ set -f
 string=$DEPLOY_SERVER
 #Iterate servers for deploy and pull last commit
       echo "Deploy project on server ${array[i]}"    
-      ssh ubuntu@$string "cd /opt && git clone http://Hariharan:SVR53S6usDk9ysxFDPB-@gitlab.com/mws-technology/MIGDataModel.git"
+      ssh ubuntu@ec2-13-232-57-200.ap-south-1.compute.amazonaws.com "cd /opt && git clone http://Hariharan:SVR53S6usDk9ysxFDPB-@gitlab.com/mws-technology/MIGDataModel.git"
